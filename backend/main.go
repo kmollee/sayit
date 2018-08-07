f9740498955536639c161ada400f46536d380342
kmollee <admin@mongkonglee.com>
2018-08-08 01:20:54 +0800

    init

---
+++ backend/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os/exec"
+
+	pb "github.com/kmollee/sayit/api"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	port := flag.Int("p", 8080, "port to listen to")
+	flag.Parse()
+
+	logrus.Infof("listening to port:%d", *port)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		logrus.Fatalf("could not listen to port %d: %v", *port, err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterTextToSpeechServer(s, server{})
+
+	if err := s.Serve(lis); err != nil {
+		logrus.Fatalf("could not serve: %v", err)
+	}
+}
+
+type server struct{}
+
+func (server) Say(ctx context.Context, text *pb.Text) (*pb.Speech, error) {
+	f, err := ioutil.TempFile("", "")
+	if err != nil {
+		return nil, fmt.Errorf("could not create tmp file: %v", err)
+	}
+	if err := f.Close(); err != nil {
+		return nil, fmt.Errorf("could not close tmp file: %v", err)
+	}
+
+	cmd := exec.Command("flite", "-t", text.Text, "-o", f.Name())
+	if data, err := cmd.CombinedOutput(); err != nil {
+		return nil, fmt.Errorf("flite fail: %s", data)
+	}
+
+	data, err := ioutil.ReadFile(f.Name())
+	if err != nil {
+		return nil, fmt.Errorf("could not write tmp file: %v", err)
+	}
+	return &pb.Speech{Audio: data}, nil
+}
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Sayit
+
+say it, transform text to audio
+
+## requirement
+
+Requirements:
+- Install [protobuf](https://github.com/google/protobuf/releases)
+
+References:
+- flite [docs](http://www.speech.cs.cmu.edu/flite/)
+- gRPC [docs](https://grpc.io)
+- Kubernetes [docs](https://kubernetes.io)
+- Google Container Engine [docs](https://cloud.google.com/container-engine/)Requirements:
+
+## refer
+
+[justforfunc 12: a Text to Speech server with gRPC and Kubernetes](https://github.com/campoy/justforfunc/tree/master/12-say-grpc)
+
+Videos:
+- justforfunc #12: a Text to Speech server with gRPC and Kubernetes [video](https://www.youtube.com/watch?v=XaMr--wAuSI)
+- justforfunc #9: the context package [video](https://www.youtube.com/watch?v=LSzR0VEraWw&index=1&list=PL64wiCrrxh4Jisi7OcCJIUpguV_f5jGnZ)
+- justforfunc #10: implementing the context package [video](https://www.youtube.com/watch?v=LSzR0VEraWw&feature=youtu.be&list=PL6)
+- GopherFest Livestream [video](https://www.youtube.com/watch?v=giUatBmmb_Y)
+
